b5585ead95299c66e817e03f9a58ceaeb0e4462f
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2023-04-16 16:06:54 +0700

    first commit

--- controllers/admin/base_controller.php
+++ controllers/admin/base_controller.php
@@ -9,7 +9,9 @@ class BaseController
         if (is_file($view_file)) {
             extract($data);
             ob_start();
+            require_once('views/admin/header.php');
             require_once('views/admin/sidebar.php');
+            require_once('views/admin/footer.php');
             require_once($view_file);
             $content = ob_get_clean();
             require_once('/xampp/htdocs/admin_iot/views/admin/basic_layouts.php');
---
+++ views/admin/footer.php
@@ -0,0 +1,19 @@
+    <script src="/admin_iot/public/vendor/jquery/jquery.min.js"></script>
+    <script src="/admin_iot/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="/admin_iot/public/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="/admin_iot/public/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="/admin_iot/public/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="/admin_iot/public/js/demo/chart-area-demo.js"></script>
+    <script src="/admin_iot/public/js/demo/chart-pie-demo.js"></script>
+
+    </body>
+
+    </html>
